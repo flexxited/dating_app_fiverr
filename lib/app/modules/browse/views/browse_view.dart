28d6699c063e7b2fa24ebcecbcae9eb7930f5ed5
Narayan Vyas <narayanvyas87@gmail.com>
2021-07-26 08:37:46 +0530

    Fixed Height Issue

--- lib/app/modules/browse/views/browse_view.dart
+++ lib/app/modules/browse/views/browse_view.dart
@@ -1,12 +1,74 @@
 import 'package:flutter/material.dart';
 import 'package:hexcolor/hexcolor.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 
 String primaryColor = '#090615';
 String secondaryColor = '#2E2D3A';
 
 class BrowseView extends StatelessWidget {
+  final List gridItems = [
+    {
+      'name': 'Iris',
+      'age': 22,
+      'title': 'Entrepreneur',
+      'image_url':
+          'https://images.pexels.com/photos/1987301/pexels-photo-1987301.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'
+    },
+    {
+      'name': 'Amina',
+      'age': 20,
+      'title': 'Data Analysis',
+      'image_url':
+          'https://images.pexels.com/photos/3142449/pexels-photo-3142449.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'
+    },
+    {
+      'name': 'Natali',
+      'age': 28,
+      'title': 'Fashion Designer',
+      'image_url':
+          'https://images.pexels.com/photos/1580270/pexels-photo-1580270.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'
+    },
+    {
+      'name': 'Becky',
+      'age': 25,
+      'title': 'Student',
+      'image_url':
+          'https://images.pexels.com/photos/1372134/pexels-photo-1372134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'
+    },
+    {
+      'name': 'Cara',
+      'age': 21,
+      'title': 'Accountant',
+      'image_url':
+          'https://images.pexels.com/photos/1386604/pexels-photo-1386604.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'
+    },
+    {
+      'name': 'Mithechel',
+      'age': 20,
+      'title': 'Teacher',
+      'image_url':
+          'https://images.pexels.com/photos/1391499/pexels-photo-1391499.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'
+    },
+    {
+      'name': 'Samantha',
+      'age': 21,
+      'title': 'Business',
+      'image_url':
+          'https://images.pexels.com/photos/1468379/pexels-photo-1468379.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'
+    },
+    {
+      'name': 'Karla',
+      'age': 20,
+      'title': 'Teacher',
+      'image_url':
+          'https://images.pexels.com/photos/1391499/pexels-photo-1391499.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'
+    },
+  ];
   @override
   Widget build(BuildContext context) {
+    var size = MediaQuery.of(context).size;
+    final double itemHeight = (size.height - kToolbarHeight - 24) / 3.2;
+    final double itemWidth = size.width / 2;
     return Scaffold(
       appBar: AppBar(
         backgroundColor: HexColor(primaryColor),
@@ -32,32 +94,21 @@ class BrowseView extends StatelessWidget {
               SizedBox(
                 height: 30,
               ),
-              GridView(
+              StaggeredGridView.countBuilder(
+                itemCount: gridItems.length,
                 shrinkWrap: true,
                 physics: ScrollPhysics(),
-                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisSpacing: 7,
-                    childAspectRatio: 1.65 / 2,
-                    mainAxisSpacing: 10,
-                    crossAxisCount: 2),
-                children: [
-                  generateGridItem('Iris', 22, 'Entrepreneur',
-                      'https://images.pexels.com/photos/1987301/pexels-photo-1987301.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'),
-                  generateGridItem('Amina', 20, 'Data Analysis',
-                      'https://images.pexels.com/photos/3142449/pexels-photo-3142449.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'),
-                  generateGridItem('Natali', 28, 'Fashion Designer',
-                      'https://images.pexels.com/photos/1580270/pexels-photo-1580270.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'),
-                  generateGridItem('Becky', 25, 'Student',
-                      'https://images.pexels.com/photos/1372134/pexels-photo-1372134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'),
-                  generateGridItem('Cara', 21, 'Accountant',
-                      'https://images.pexels.com/photos/1386604/pexels-photo-1386604.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'),
-                  generateGridItem('Mithechel', 20, 'Teacher',
-                      'https://images.pexels.com/photos/1391499/pexels-photo-1391499.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'),
-                  generateGridItem('Samantha', 21, 'Business',
-                      'https://images.pexels.com/photos/1468379/pexels-photo-1468379.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'),
-                  generateGridItem('Karla', 20, 'Teacher',
-                      'https://images.pexels.com/photos/1391499/pexels-photo-1391499.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'),
-                ],
+                staggeredTileBuilder: (index) => StaggeredTile.fit(1),
+                crossAxisSpacing: 7,
+                mainAxisSpacing: 10,
+                crossAxisCount: 2,
+                itemBuilder: (context, index) {
+                  return generateGridItem(
+                      gridItems[index]['name'],
+                      gridItems[index]['age'],
+                      gridItems[index]['title'],
+                      gridItems[index]['image_url']);
+                },
               )
             ],
           ),
